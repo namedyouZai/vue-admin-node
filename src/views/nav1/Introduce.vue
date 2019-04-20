2ef586a02d81db6a3e2613b97270573062cd8b52
namedyouZai <272071977@qq.com>
2019-04-20 17:00:29 +0800

    增加页面

--- src/views/nav1/Introduce.vue
+++ src/views/nav1/Introduce.vue
@@ -1,9 +1,10 @@
 <template>
 	<section>
-		<el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
-			孙艺晖
-			2015213456
-
-		</el-col>
+		<div>
+			<el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+				孙艺晖
+				2015213456
+			</el-col>
+		</div>
 	</section>
 </template>
